afcc1f7fcdaddbe73b984d01a7dc365474ec0c30
alleyesunk <53035043+alleyesunk@users.noreply.github.com>
2020-12-16 09:56:00 +0800

    Add files via upload

---
+++ haha.js
@@ -0,0 +1,41 @@
+// 定义变量和函数 
+const customName = document.getElementById('customname'); 
+const randomize = document.querySelector('.randomize'); 
+const story = document.querySelector('.story'); 
+function randomValueFromArray(array){
+  const random = Math.floor(Math.random() * array.length);
+  return array[random];
+}
+//文本字符串
+let storyText = '今天气温 35 摄氏度，:insertx:出门散步。当走到:inserty:门前时，突然就:insertz:。人们都惊呆了，李雷全程目睹但并没有慌，因为:insertx:是一个 140 公斤的胖子，天气又辣么热。';
+let insertX = ['怪兽威利', '大老爹', '圣诞老人'];  
+let insertY = ['肯德基', '迪士尼乐园', '白宫'];
+let insertZ = ['自燃了', '在人行道化成了一坨泥', '变成一只鼻涕虫爬走了'];
+//监听按钮点击事件
+randomize.addEventListener('click', result);
+
+function result() {
+  let newStory = storyText;
+  let xItem = randomValueFromArray(insertX);
+  let yItem = randomValueFromArray(insertY);
+  let zItem = randomValueFromArray(insertZ);
+  newStory = newStory.replace(':insertx:', xItem);
+  newStory = newStory.replace(':insertx:', xItem);
+  newStory = newStory.replace(':inserty:', yItem);
+  newStory = newStory.replace(':insertz:', zItem);
+//判断输入框是否为空，不为空则进行赋值
+  if(customName.value !== '') {
+    const name = customName.value;
+    newStory = newStory.replace('李雷', name);
+  }
+//判断选择框
+  if(document.getElementById("american").checked) {
+    const weight = Math.round(140 * 2.20462) + ' 磅';
+    const temperature =  Math.round(35 * 9 / 5 + 32) + ' 华氏度';
+    newStory = newStory.replace('35 摄氏度', temperature);
+    newStory = newStory.replace('140 公斤', weight);
+  }
+//输出内容让其可见
+  story.textContent = newStory;
+  story.style.visibility = 'visible';
+}
